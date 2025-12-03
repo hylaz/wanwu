ffcd3ff377eeff5e0cb94b53ab71e44c15a4cc31
dysiliky <dysiliky@163.com>
2025-12-03 18:21:38 +0800

    style(wanwu): 优化配置国际化代码格式

--- web/src/lang/index.js
+++ web/src/lang/index.js
@@ -9,21 +9,21 @@ import {ZH} from "@/lang/constants"
 Vue.use(VueI18n)
 
 const messages = {
-    zh: {
-        language: '简体中文',
-        ...zh,
-        ...zhLocale
-    },
-    en: {
-        language: 'English',
-        ...en,
-        ...enLocale
-    },
+  zh: {
+    language: '简体中文',
+    ...zh,
+    ...zhLocale
+  },
+  en: {
+    language: 'English',
+    ...en,
+    ...enLocale
+  },
 }
 const i18n = new VueI18n({
-    locale: localStorage.getItem('locale') || ZH, // 目前默认中文，语言类型存储到 localstorage 里
-    messages
+  locale: localStorage.getItem('locale') || ZH, // 目前默认中文，语言类型存储到 localstorage 里
+  messages
 })
 
 // 导出 messages 给切换语言的时候用
-export { i18n, messages }
+export {i18n, messages}
